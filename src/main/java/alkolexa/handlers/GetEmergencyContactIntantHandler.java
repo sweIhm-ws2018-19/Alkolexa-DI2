33e139c81b7306d88ab087e49da738c2f0d4b11f
Marvin Pönisch <mpoenisc@hm.edu>
2018-11-20 13:40:04 +0100

    add EmergencyContactIntentHandler

---
+++ src/main/java/alkolexa/handlers/GetEmergencyContactIntantHandler.java
@@ -0,0 +1,36 @@
+package main.java.alkolexa.handlers;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class GetEmergencyContactIntantHandler  implements RequestHandler{
+	public static final String CONTACT_KEY = "CONTACT";
+	public static final String CONTACT_SLOT = "Contact";
+
+	@Override
+	public boolean canHandle(HandlerInput input) {
+		return input.matches(intentName("GetEmergencyContactIntent"));
+	}
+
+	@Override
+	public Optional<Response> handle(HandlerInput input) {
+		String speechText;
+		String favoriteColor = (String) input.getAttributesManager().getSessionAttributes().get(CONTACT_KEY);
+		// till jet just saved for session
+		// TODO: access to DynamoDB
+
+		if (favoriteColor != null && !favoriteColor.isEmpty()) {
+			speechText = String.format("Dein Notfallkontakt ist %s.", favoriteColor);
+		} else {
+			// Since the user's favorite color is not set render an error message.
+			speechText = "Ich weiss nicht wer Dein Notfallkontakt ist. Sag mir Dein Notfallkontakt. Sage zum Beispiel: Mein Notfallkontakt ist Max Mustermann.";
+		}
+
+		return input.getResponseBuilder().withSpeech(speechText).withSimpleCard("AlkolexaSession", speechText).build();
+	}
+}
