da3d688460da700f445847e909fc2dc0170deb9b
DerAlexx <alexweb199@hotmail.de>
2018-11-24 20:19:38 +0100

    lastest Version

---
+++ src/main/java/alkolexa/alkolexaHandlers/NavigateHomeIntentHandler.java
@@ -0,0 +1,23 @@
+package main.java.alkolexa.alkolexaHandlers;
+
+import java.util.Optional;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+public class NavigateHomeIntentHandler implements RequestHandler {
+
+	@Override
+	public boolean canHandle(HandlerInput input) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Optional<Response> handle(HandlerInput input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
