61d3a3c437679185a03940c96d30ca2dccbc27bd
WagnerThomas <43779014+WagnerThomas@users.noreply.github.com>
2018-11-24 21:29:42 +0100

    Add files via upload

--- src/main/java/alkolexa/AlkolexaStreamHandler.java
+++ src/main/java/alkolexa/AlkolexaStreamHandler.java
@@ -1,14 +1,27 @@
-package main.java.alkolexa;
+/*
+     Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+     Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+     except in compliance with the License. A copy of the License is located at
+
+         http://aws.amazon.com/apache2.0/
+
+     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+     the specific language governing permissions and limitations under the License.
+*/
+
+package alkolexa;
 
 import com.amazon.ask.Skill;
 import com.amazon.ask.SkillStreamHandler;
 import com.amazon.ask.Skills;
-import main.java.alkolexa.alkolexaHandlers.*;
 
-
-/**
- * org.apache.maven.plugins:maven-assembly-plugin:2.6:assembly -DdescriptorId=jar-with-dependencies package
- */
+import alkolexa.handlers.CancelandStopIntentHandler;
+import alkolexa.handlers.FallbackIntentHandler;
+import alkolexa.handlers.HelpIntentHandler;
+import alkolexa.handlers.LaunchRequestHandler;
+import alkolexa.handlers.SessionEndedRequestHandler;
 
 public class AlkolexaStreamHandler extends SkillStreamHandler {
 
@@ -20,12 +33,12 @@ public class AlkolexaStreamHandler extends SkillStreamHandler {
                         new SessionEndedRequestHandler(),
                         new HelpIntentHandler(),
                         new FallbackIntentHandler())
-                .withSkillId("amzn1.ask.skill.6d0140e1-4583-4cc6-907b-4666dff6293c")
+                .withSkillId("amzn1.ask.skill.65f49a2f-9e3f-47a7-84c2-228f138002be")
                 .build();
     }
-
 
     public AlkolexaStreamHandler() {
         super(getSkill());
     }
+
 }
