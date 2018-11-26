c4962e24f9ea743eb75edbe52570ef9370c1a46d
Anian <Anian@ANIAN-SURFACE>
2018-11-26 15:24:56 +0100

    declared functions static

--- src/main/java/alkolexa/api/CocktailAPI.java
+++ src/main/java/alkolexa/api/CocktailAPI.java
@@ -17,19 +17,19 @@ import javax.json.JsonObject;
 
 public class CocktailAPI {
 
-	public CocktailAPI() {
-
-		// Simple Basic Tests
-		// JsonObject cocktail = searchForCocktail("magerita");
-		// System.out.println(getCocktailName(cocktail));
-		// System.out.println(getCocktailInstructions(cocktail));
-		// System.out.println(getCocktailIngredients(cocktail));
-		// System.out.println(getCocktailMeasures(cocktail));
-
-	}
+//	public CocktailAPI() {
+//
+//		// Simple Basic Tests
+//		// JsonObject cocktail = searchForCocktail("magerita");
+//		// System.out.println(getCocktailName(cocktail));
+//		// System.out.println(getCocktailInstructions(cocktail));
+//		// System.out.println(getCocktailIngredients(cocktail));
+//		// System.out.println(getCocktailMeasures(cocktail));
+//
+//	}
 
 	// Returns the First found Cocktail
-	public JsonObject searchForCocktail(String searchCocktail) {
+	public static JsonObject searchForCocktail(String searchCocktail) {
 		String url = "";
 		try {
 			url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
@@ -46,18 +46,18 @@ public class CocktailAPI {
 		return firstFoundCocktail;
 	}
 
-	public String getCocktailName(JsonObject cocktailJson) {
+	public static String getCocktailName(JsonObject cocktailJson) {
 		String name = cocktailJson.getString("strDrink");
 		return name;
 	}
 
 	// Returns the Instructions of a given Cocktail
-	public String getCocktailInstructions(JsonObject cocktailJson) {
+	public static String getCocktailInstructions(JsonObject cocktailJson) {
 		String instructions = cocktailJson.getString("strInstructions");
 		return instructions;
 	}
 
-	public ArrayList<String> getCocktailIngredients(JsonObject cocktailJson) {
+	public static ArrayList<String> getCocktailIngredients(JsonObject cocktailJson) {
 		ArrayList<String> ingredients = new ArrayList<>();
 		int currentIngredientCount = 1;
 		while (!cocktailJson.getString("strIngredient" + currentIngredientCount).isEmpty()) {
@@ -67,7 +67,7 @@ public class CocktailAPI {
 		return ingredients;
 	}
 
-	public ArrayList<String> getCocktailMeasures(JsonObject cocktailJson) {
+	public static ArrayList<String> getCocktailMeasures(JsonObject cocktailJson) {
 		ArrayList<String> measures = new ArrayList<>();
 		int currentMeasuresCount = 1;
 		while (!cocktailJson.getString("strMeasure" + currentMeasuresCount).isEmpty()) {
@@ -77,11 +77,6 @@ public class CocktailAPI {
 			currentMeasuresCount++;
 		}
 		return measures;
-	}
-
-	public static void main(String[] args) {
-		new CocktailAPI();
-
 	}
 
 }
