63fe6caf975d7992416ca53712d7b4d009ea8fc4
Marvin Pönisch <mpoenisc@hm.edu>
2018-12-09 15:06:21 +0100

    add Favorite

---
+++ src/main/java/alkolexa/handlers/GetFavoriteIntentHandler.java
@@ -0,0 +1,31 @@
+package alkolexa.handlers;
+
+import static com.amazon.ask.request.Predicates.intentName;
+import java.util.Optional;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+
+public class GetFavoriteIntentHandler implements RequestHandler {
+	public static final String FAVORITE_KEY = "FAVORITE";
+	public static final String FAVORITE_SLOT = "Favorite";
+
+	@Override
+	public boolean canHandle(HandlerInput input) {
+		return input.matches(intentName("GetFavorite"));
+	}
+
+	@Override
+	public Optional<Response> handle(HandlerInput input) {
+		String speechText;
+		String favorite = (String) input.getAttributesManager().getSessionAttributes().get(FAVORITE_KEY);
+		if (favorite != null && !favorite.isEmpty())
+			speechText = String.format("Dein Favorite Cocktail ist %s.", favorite);
+		else
+			speechText = "Ich kenne dein Favorite Cocktail nicht. Sag mir dein Favorite Cocktail. Sage zum Beispiel: Mein Favorite Cocktail ist Mochito.";
+		return input.getResponseBuilder() //
+				.withSpeech(speechText) //
+				.withReprompt("") //
+				.build();
+	}
+}
---
+++ src/main/java/alkolexa/handlers/SetFavoriteIntentHandler.java
@@ -0,0 +1,68 @@
+package alkolexa.handlers;
+
+import static com.amazon.ask.request.Predicates.intentName;
+import java.util.Optional;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Intent;
+import com.amazon.ask.model.IntentRequest;
+import com.amazon.ask.model.Request;
+
+import com.amazon.ask.model.Slot;
+import com.amazon.ask.response.ResponseBuilder;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import static alkolexa.handlers.GetFavoriteIntentHandler.FAVORITE_KEY;
+import static alkolexa.handlers.GetFavoriteIntentHandler.FAVORITE_SLOT;
+
+public class SetFavoriteIntentHandler implements RequestHandler {
+	@Override
+	public boolean canHandle(HandlerInput input) {
+		return input.matches(intentName("SetFavorite"));
+	}
+
+	@Override
+	public Optional<Response> handle(HandlerInput input) {
+		Request request = input.getRequestEnvelope().getRequest();
+		IntentRequest intentRequest = (IntentRequest) request;
+		Intent intent = intentRequest.getIntent();
+		Map<String, Slot> slots = intent.getSlots();
+
+		Slot favoriteCocktailSlot = slots.get(FAVORITE_SLOT);
+
+		String speechText, repromptText;
+		boolean isAskResponse = false;
+
+		if (favoriteCocktailSlot != null) {
+			String favoriteCocktail = favoriteCocktailSlot.getValue();
+			input.getAttributesManager().setSessionAttributes(Collections.singletonMap(FAVORITE_KEY, favoriteCocktail));
+
+			speechText = String.format(
+					"Deine Favorite Cocktail ist %s. Du kannst mich jetzt nach Deinem Favorite Cocktail fragen. "
+							+ "Frage einfach: was ist mein Favorite Cocktail?",
+					favoriteCocktail);
+			repromptText = "Frage nach meinem Favorite Cocktail.";
+
+		} else {
+			speechText = "Ich kenne Dein Favorite Cocktail nicht. Bitte versuche es noch einmal.";
+			repromptText = "Ich weiss nicht welches Dein Favorite Cocktail ist. Sag mir Dein Favorite Cocktail. Sage zum Beispiel: Mein Favorite Cocktail ist Mochito.";
+			isAskResponse = true;
+		}
+
+		ResponseBuilder responseBuilder = input.getResponseBuilder();
+
+		responseBuilder.withSimpleCard("", speechText)//
+				.withSpeech(speechText) //
+				.withShouldEndSession(false); // Some error Messages are missing
+
+		if (isAskResponse) {
+			responseBuilder.withShouldEndSession(false).withReprompt(repromptText);
+		}
+
+		return responseBuilder.build();
+	}
+}
