eca99e49d5ee2a86aa1153038bcce01022196ade
Anian <Anian@ANIAN-SURFACE>
2018-12-11 02:04:01 +0100

    implement PersistentSaver

---
+++ src/main/java/alkolexa/model/PersistentSaver.java
@@ -0,0 +1,34 @@
+package alkolexa.model;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import javax.json.JsonObject;
+
+public class PersistentSaver {
+	private static final String URL = "http://alkolexa.awsh-dev.de/";
+	
+	public static void setFavorite(String favName) {
+	
+		try {
+			favName = URLEncoder.encode(favName, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		JsonObject jsonObj = JsonObjectFromUrl.getJsonObjectFromUrl(URL+"?name="+favName);
+		String status = jsonObj.get("status").toString().replace("\"","");
+		if(!status.equals("success")) {
+			System.out.println("coudnt store favourite");
+		}
+		
+		
+	}
+	
+	public static String getFavorite() {
+		JsonObject jsonObj = JsonObjectFromUrl.getJsonObjectFromUrl(URL);
+		String fav = jsonObj.get("name").toString().replace("\"", "");
+		return fav;
+	}
+
+}
