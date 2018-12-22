2c115fcaea0bf96137a3f5ca8d4cf4ac214efbfd
Anian <Anian@ANIAN-SURFACE>
2018-12-22 12:16:19 +0100

    prepare vor sonarcloud

--- src/main/java/alkolexa/model/PersistentSaver.java
+++ src/main/java/alkolexa/model/PersistentSaver.java
@@ -10,33 +10,38 @@ package alkolexa.model;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.json.JsonObject;
 
 public class PersistentSaver {
 	private static final String URL = "http://alkolexa.awsh-dev.de/";
-	
+	private static final Logger logger = Logger.getLogger(API.class.getName());
+	private PersistentSaver() {
+
+	}
+
 	public static void setFavorite(String favName) {
-	
+
 		try {
 			favName = URLEncoder.encode(favName, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+			logger.log(Level.WARNING, "Exception");
 		}
-		
-		JsonObject jsonObj = JsonObjectFromUrl.getJsonObjectFromUrl(URL+"?name="+favName);
-		String status = jsonObj.get("status").toString().replace("\"","");
-		if(!status.equals("success")) {
+
+		JsonObject jsonObj = JsonObjectFromUrl.getJsonObjectFromUrl(URL + "?name=" + favName);
+		String status = jsonObj.get("status").toString().replace("\"", "");
+		if (!status.equals("success")) {
 			System.out.println("coudnt store favourite");
 		}
-		
-		
+
 	}
-	
+
 	public static String getFavorite() {
 		JsonObject jsonObj = JsonObjectFromUrl.getJsonObjectFromUrl(URL);
-		String fav = jsonObj.get("name").toString().replace("\"", "");
-		return fav;
+
+		return jsonObj.get("name").toString().replace("\"", "");
 	}
 
 }
