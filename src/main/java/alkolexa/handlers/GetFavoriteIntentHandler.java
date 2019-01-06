ce31abd9338e3f47f89fa2d7b882dcce4d727ffd
DerAlexx <alexweb199@hotmail.de>
2019-01-06 15:44:04 +0100

    fixed small things

--- src/main/java/alkolexa/handlers/GetFavoriteIntentHandler.java
+++ src/main/java/alkolexa/handlers/GetFavoriteIntentHandler.java
@@ -5,15 +5,9 @@ import java.util.Optional;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 import com.amazon.ask.model.Response;
-
-import alkolexa.SpeechStrings;
-import alkolexa.model.API;
 import alkolexa.model.PersistentSaver;
 
 public class GetFavoriteIntentHandler implements RequestHandler {
-	public static final String FAVORITE_KEY = "FAVORITE";
-	public static final String FAVORITE_SLOT = "Favorite";
-
 	/**
 	 * Method to Handle a Input.
 	 */
@@ -30,7 +24,7 @@ public class GetFavoriteIntentHandler implements RequestHandler {
 		String favorite = PersistentSaver.getFavorite();
 		if(favorite == null || favorite.equals("") || favorite.length() < 2) {
 			return input.getResponseBuilder()
-					.withSpeech("Du hast noch kein Favorite Cocktail hinzugefügt")
+					.withSpeech("Du hast noch kein lieblings Cocktail hinzugefuegt")
 					.withShouldEndSession(false)
 					.build();
 		} else {
